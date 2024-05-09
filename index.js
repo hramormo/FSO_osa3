4493d22a4c5473a35e078fbff181632dd4d1ab84
hramormo <sekinviela@hotmail.com>
2024-05-09 22:39:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
